0b01ae11b11eb0ed4872f39ec6dadbcb5cfbf5de
Elizabeth Garcia-Quilinquin <equilinquin@gmail.com>
2020-01-05 20:58:03 -0800

    added json package

--- README.md
+++ README.md
@@ -13,4 +13,4 @@ The PDF will be populated with the following:
 * Number of GitHub stars
 * Number of users following
 
-comment
+This application works in Node.js.  It will populate information from prompts and user input into the terminal, using the inquirer package, to create a Node-Profile using GitHub.  Once user provides their GitHub username and color (from choices provided), it will generate an HTML called 'profile.html' and the user can open the file to view their generated GitHub profile.  In this generated profile, it will collect the user profile image, name, location, bio, blog(profile), amount of repos, amount of followers, amount of starred repos, and amount of people they follow.  Each color shows a different layout of the page.  Once information is collected, the js file will convert the html file to a pdf file as the final product.
--- index.js
+++ index.js
@@ -1,69 +1,72 @@
 const inquirer = require("inquirer");
-const gi =  require("gitignore")
 const fs = require("fs");
-     convertFactory = require("electron-html-to");
+const electron = require("electron");
+const convertFactory = require('electron-html-to');
 const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 const axios = require("axios");
 const generateHTML = require("./generateHTML");
+const gi = require("./gitignore");
+
+const questions = [
+  {
+    type: "input",
+    name: "username",
+    message: "What is your GitHub username?"
+  },
+  {
+    type: "input",
+    name: "color",
+    message: "Do you like green, blue, pink, or red? (Choose One)"
+  }
+];
+function promptUser() {
+  return inquirer.prompt(questions);
+}
+
+promptUser().then(ans => {
+  const one = axios.get(`https://api.github.com/users/${ans.username}`);
+  const two = axios.get(`https://api.github.com/users/${ans.username}/watched`);
+
+  axios
+    .all([one, two])
+    .then(
+      axios.spread((...res) => {
+        const resOne = res[0];
+        const resTwo = res[1];
+
+        writeToFile(
+          "profile.html",
+          generateHTML(resOne.data, resTwo.data, ans.color)
+        );
+      })
+    )
+    .catch(err => {
+      if (err) {
+        throw err;
+      }
+    });
+});
+
+
+
 const conversion = convertFactory({
   converterPath: convertFactory.converters.PDF,
   allowLocalFilesAccess: true
 });
 
-const questions = [    
-    {
-      type: "input",
-      name: "username",
-      message: "What is your GitHub username?"
-    },
-    {
-      type: "input",
-      name: "color",
-      message: "Do you like green, blue, pink, or red? (Choose One)"
-    }
-  
-];
-function promptUser() {
-  return inquirer.prompt(questions);
-};
-
-promptUser().then(ans=>{
-  const one = axios.get(`https://api.github.com/users/${ans.username}`);
-  const two = axios.get(`https://api.github.com/users/${ans.username}/watched`);
-
-  axios.all([one, two]).then(axios.spread((...res) => {
-    const resOne = res[0];
-    const resTwo = res[1];
-
-    writeToFile('profile.html', generateHTML(resOne.data, resTwo.data, ans.color))
-  })).catch((err) => {
-    if(err) {
-      throw(err);
-    };
-  });
-
-});
-
-
- function writeToFile(fileName, data) {
-writeFileAsync(fileName, data).then(()=>{
-    let htmlStr = fs.readFile('./profile.html', 'utf8', (err, data)=>{
-        if(err) {
-            throw err;
-        }
+function writeToFile(fileName, data) {
+  writeFileAsync(fileName, data).then(() => {
+    conversion({ html: "./profile.html" }, function(err, result) {
+      if (err) {
+        return console.error(err);
+      }
+      console.log(result.numberOfPages);
+      console.log(result.logs);
+      result.stream.pipe(fs.createWriteStream("profile.pdf"));
+      conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
     });
-    conversion({html: htmlStr}, function(err, result){
-            if (err) {
-                return console.error(err);
-              }
-              console.log(result.numberOfPages);
-              console.log(result.logs);
-              result.stream.pipe(fs.createWriteStream('profile.pdf'));
-              conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
-            
-            });
 
-      //writeFileAsync('profile.pdf', data)
-});
+    //writeFileAsync('profile.pdf', data)
+  });
 }
